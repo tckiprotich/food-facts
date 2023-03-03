038b56e53cb69813fbe9261213a033df8a889f74
COLLINS  TONUI <tckiprotich@gmail.com>
2023-03-03 11:06:52 +0000

    jhgf

--- routes/home.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-router.get('/', (req, res) => {
-    console.log('GET / (home)');
-    // send index.html from the views folder
-    res.sendFile('index.html', { root: 'Views' });
-
-});
-module.exports = router;
