2f334fee3e4695bae48ee24034a4796d36ba2718
COLLINS  TONUI <tckiprotich@gmail.com>
2023-03-04 20:16:15 +0000

    open ai api search

--- pages/scanresult.js
+++ pages/scanresult.js
@@ -1,13 +1,56 @@
+// import { useRouter } from "next/router";
+
+// const ScanResultPage = () => {
+//   const router = useRouter();
+//   const { code } = router.query;
+
+//   return (
+//     <div>
+//       <h1>Scan Result</h1>
+//       <p>Barcode code: {code}</p>
+//     </div>
+//   );
+// };
+
+// export default ScanResultPage;
+
+
+import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 
 const ScanResultPage = () => {
   const router = useRouter();
   const { code } = router.query;
+  const [searchResults, setSearchResults] = useState(null);
+
+  useEffect(() => {
+    if (!code) {
+      return;
+    }
+
+    const searchUrl = `https://world.openfoodfacts.org/api/v0/product/${code}.json`;
+
+    fetch(searchUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setSearchResults(data.product);
+      })
+      .catch((error) => console.error(error));
+  }, [code]);
 
   return (
     <div>
       <h1>Scan Result</h1>
       <p>Barcode code: {code}</p>
+      {searchResults && (
+        <div>
+          <h2>Search Results</h2>
+          <p>Name: {searchResults.product_name}</p>
+          <p>Brand: {searchResults.brands}</p>
+          <p>Ingredients: {searchResults.ingredients_text}</p>
+        </div>
+      )}
     </div>
   );
 };
