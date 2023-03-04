93a44ffb831d893c976728e3ab3ea3709f057a7b
COLLINS  TONUI <tckiprotich@gmail.com>
2023-03-04 21:27:50 +0000

    FF

--- pages/scanresult.js
+++ pages/scanresult.js
@@ -1,41 +1,33 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import axios from "axios";
-import { config } from "dotenv";
-config();
 
 const ScanResultPage = () => {
   const router = useRouter();
   const { code } = router.query;
   const [product, setProduct] = useState(null);
-  const [nutrients, setNutrients] = useState(null);
 
   useEffect(() => {
     if (code) {
-      // Search Open Food Facts API for product data
       axios
         .get(`https://world.openfoodfacts.org/api/v0/product/${code}.json`)
         .then((response) => {
           const product = response.data.product;
           setProduct(product);
-
-          // If product not found, search USDA National Nutrient Database
-          if (!product) {
-            axios
-              .get(
-                `https://api.nal.usda.gov/fdc/v1/foods/search?query=${code}&pageSize=1&api_key=${process.env.USDA_API_KEY}`
-              )
-              .then((response) => {
-                const food = response.data.foods[0];
-                setNutrients(food.foodNutrients);
-              })
-              .catch((error) => {
-                console.error(error);
-              });
-          }
         })
         .catch((error) => {
           console.error(error);
+          axios
+            .get(
+              `https://api.nal.usda.gov/fdc/v1/foods/search?query=${code}&pageSize=1&api_key=8xHruEEQncI7q81n2BheQHqvTq4snh46rRGud6cj`
+            )
+            .then((response) => {
+              const product = response.data.foods[0];
+              setProduct(product);
+            })
+            .catch((error) => {
+              console.error(error);
+            });
         });
     }
   }, [code]);
@@ -44,30 +36,27 @@ const ScanResultPage = () => {
     return <p>Loading...</p>;
   }
 
-  if (!product && !nutrients) {
+  if (!product) {
     return <p>No product found for barcode {code}.</p>;
+  }
+
+  if (product.brands === "USDA") {
+    return (
+      <div>
+        <h1>{product.description}</h1>
+        <p>{product.foodNutrients[3].nutrientName}: {product.foodNutrients[3].value}{product.foodNutrients[3].unitName}</p>
+        <p>{product.foodNutrients[1].nutrientName}: {product.foodNutrients[1].value}{product.foodNutrients[1].unitName}</p>
+        <p>{product.foodNutrients[2].nutrientName}: {product.foodNutrients[2].value}{product.foodNutrients[2].unitName}</p>
+      </div>
+    );
   }
 
   return (
     <div>
-      {product && (
-        <>
-          <h1>{product.product_name}</h1>
-          <p>{product.generic_name}</p>
-          <p>{product.ingredients_text}</p>
-          <img src={product.image_url} alt={product.product_name} />
-        </>
-      )}
-      {nutrients && (
-        <>
-          <h1>{nutrients.name}</h1>
-          {nutrients.foodNutrient.map((nutrient) => (
-            <p key={nutrient.nutrient.name}>
-              {nutrient.nutrient.name}: {nutrient.amount}{nutrient.nutrient.unitName}
-            </p>
-          ))}
-        </>
-      )}
+      <h1>{product.product_name}</h1>
+      <p>{product.generic_name}</p>
+      <p>{product.ingredients_text}</p>
+      <img src={product.image_url} alt={product.product_name} />
     </div>
   );
 };
