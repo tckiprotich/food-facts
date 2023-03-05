10f28dfeae739fe3c64bc0ce286bf3811faae602
COLLINS  TONUI <tckiprotich@gmail.com>
2023-03-05 00:20:22 +0000

    index

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,7 @@ export default function Index() {
     <div class="container px-6 py-40 mx-auto text-center">
         <div class="max-w-lg mx-auto">
             <h1 class="text-3xl font-semibold text-gray-800 dark:text-white lg:text-4xl">Food facts  </h1>
-            <p class="mt-6 text-gray-500 dark:text-gray-300">Scan any product's barcode and get detailed information about the product including name, ingredients, labels, and image..</p>
-
+            <p className="mt-6 text-gray-500 dark:text-gray-300">Scan any products barcode and get detailed information about the product including name, ingredients, labels, and image..</p>
             < Link href="/scan">
             <button class="px-5 py-2 mt-6 text-sm font-medium leading-5 text-center text-white capitalize bg-blue-600 rounded-lg hover:bg-blue-500 lg:mx-0 lg:w-auto focus:outline-none">
                 Scan Barcode
--- pages/scanresult.js
+++ pages/scanresult.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import axios from "axios";
+import Image from "next/image";
+import Link from "next/link";
 
 const ScanResultPage = () => {
   const router = useRouter();
